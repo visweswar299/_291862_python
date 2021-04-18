f1ed1b8a0cd1c711623d45738fe8210339b31eba
visweswar <visweswar@299@gmail.com>
2021-04-18 18:29:02 +0530

    Membership operations

---
+++ Membership operators.py
@@ -0,0 +1,21 @@
+print("""
+Membership operators
+""")
+
+print("""
+in
+""")
+print("""
+Returns True if a sequence with the specified value is present in the object
+""")
+x = ["apple", "banana"]
+print("banana" in x)
+
+print("""
+not in
+""")
+print("""
+Returns True if a sequence with the specified value is not present in the object
+""")
+x=["apple","banana"]
+print("apple" not in x)
