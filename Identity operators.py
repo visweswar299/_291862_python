3448e38b6d171cbbd82cec404e5b394997fddba3
visweswar <visweswar@299@gmail.com>
2021-04-18 18:25:57 +0530

    Identity operations

---
+++ Identity operators.py
@@ -0,0 +1,23 @@
+print("""
+Identity operators
+""")
+
+print("""
+is
+""")
+print("""
+Returns True if both variables are the same object
+""")
+x=3
+y=3
+print(x is y)
+
+print("""
+is not
+""")
+print("""
+Returns True if both variables are not the same object
+""")
+x=3
+y=3
+print(x is not y)
