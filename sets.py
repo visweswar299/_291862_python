e2bf5432fa0c3bcaf7022b7d40510a4cb010d5cb
visweswar <visweswar@299@gmail.com>
2021-04-20 07:55:49 +0530

    sets update

--- sets.py
+++ sets.py
@@ -1,2 +1,185 @@
 myset = {"apple", "banana", "cherry"}
 print(myset)
+
+'''
+Sets are used to store multiple items in a single variable.
+
+Set is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Tuple, and Dictionary, all with different qualities and usage.
+
+A set is a collection which is both unordered and unindexed.
+
+Sets are written with curly brackets.
+
+Set items are unordered, unchangeable, and do not allow duplicate values.
+
+'''
+thisset = {"apple", "banana", "cherry", "apple"}
+
+print(thisset)
+print(len(thisset))
+
+#String, int and boolean data types:
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
+set1 = {"abc", 34, True, 40, "male"}
+
+print(type(set1))
+
+#set constructor
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
+
+'''
+You cannot access items in a set by referring to an index or a key.
+
+But you can loop through the set items using a for loop, or ask if a specified value is present in a set, by using the in keyword.
+'''
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+     print(x)
+
+print("banana" in thisset)
+
+'''
+Once a set is created, you cannot change its items, but you can add new items.
+
+To add one item to a set use the add() method.
+'''
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+
+#Add elements from tropical and thisset into newset:
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset)
+
+#The object in the update() method does not have be a set, it can be any iterable object (tuples, lists, dictionaries etc.).
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
+
+#To remove an item in a set, use the remove(), or the discard() method
+thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset)
+#Note: If the item to remove does not exist, remove() will raise an error.
+
+'''
+You can also use the pop() method to remove an item, but this method will remove the last item. Remember that sets are unordered, so you will not know what item that gets removed.
+
+The return value of the pop() method is the removed item.
+
+Note: Sets are unordered, so when using the pop() method, you do not know which item that gets removed.
+
+'''
+
+thisset = {"apple", "banana", "cherry"}
+
+x = thisset.pop()
+
+print(x)
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.clear()
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+del thisset
+
+#print(thisset)
+
+'''
+There are several ways to join two or more sets in Python.
+
+You can use the union() method that returns a new set containing all items from both sets, or the update() method that inserts all the items from one set into another:
+
+The update() method inserts the items in set2 into set1.
+
+Note: Both union() and update() will exclude any duplicate items.
+'''
+
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+
+set3 = set1.union(set2)
+print(set3)
+
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+
+set1.update(set2)
+print(set1)
+#The intersection_update() method will keep only the items that are present in both sets.
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+x.intersection_update(y)
+
+print(x)
+#The intersection() method will return a new set, that only contains the items that are present in both sets.
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.intersection(y)
+
+print(z)
+
+#The symmetric_difference_update() method will keep only the elements that are NOT present in both sets.
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+x.symmetric_difference_update(y)
+
+print(x)
+#The symmetric_difference() method will return a new set, that contains only the elements that are NOT present in both sets.
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.symmetric_difference(y)
+
+print(z)
+
+'''
+Sets Methods:
+
+add()	            Adds an element to the set
+clear()	            Removes all the elements from the set
+copy()	            Returns a copy of the set
+difference()	    Returns a set containing the difference between two or more sets
+difference_update()	Removes the items in this set that are also included in another, specified set
+discard()	        Remove the specified item
+intersection()	    Returns a set, that is the intersection of two other sets
+intersection_update()	Removes the items in this set that are not present in other, specified set(s)
+isdisjoint()	    Returns whether two sets have a intersection or not
+issubset()	        Returns whether another set contains this set or not
+issuperset()	    Returns whether this set contains another set or not
+pop()	            Removes an element from the set
+remove()	            Removes the specified element
+symmetric_difference()	Returns a set with the symmetric differences of two sets
+symmetric_difference_update()	inserts the symmetric differences from this set and another
+union()           	Return a set containing the union of sets
+update()	        Update the set with the union of this set and others
+'''
