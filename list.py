650b1a387b344fae17a0ac4baa0af367206261cf
visweswar <visweswar@299@gmail.com>
2021-04-20 10:36:18 +0530

    list updates

--- list.py
+++ list.py
@@ -191,3 +191,60 @@ remove()	Removes the item with the specified value
 reverse()	Reverses the order of the list
 sort()	    Sorts the list
 '''
+print(
+'''
+List Comprehension
+'''
+)
+
+'''
+List comprehension offers a shorter syntax when you want to create a new list based on the values of an existing list.
+
+Example:
+
+Based on a list of fruits, you want a new list, containing only the fruits with the letter "a" in the name.
+
+'''
+
+#Without list comprehension you will have to write a for statement with a conditional test inside:
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+
+    if "a" in x:
+
+        newlist.append(x)
+
+print(newlist)
+
+#With list comprehension you can do all that with only one line of code:
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]
+
+print(newlist)
+
+#Only accept items that are not "apple":
+
+newlist = [x for x in fruits if x != "apple"]
+print(newlist)
+
+#With no if statement:
+
+newlist = [x for x in fruits]
+print(newlist)
+
+#The iterable can be any iterable object, like a list, tuple, set etc.
+
+#You can use the range() function to create an iterable:
+
+newlist = [x for x in range(10)]
+print(newlist)
+newlist = [x for x in range(10) if x<5]
+print(newlist)
+#Set the values in the new list to upper case:
+
+newlist = [x.upper() for x in fruits]
+print(newlist)
+
