bf8ec15cca62f552ec908f04251bbb3b1228c08b
visweswar <visweswar@299@gmail.com>
2021-04-23 09:03:13 +0530

    list unsolved2 solution update

--- List unsolved2.py
+++ List unsolved2.py
@@ -1,8 +1,7 @@
 #write a python program to change the position of every n-th value with the (n+1)th in a list
 
-def change_position(list):
-    for i in range(0,len(list)):
-        list[i],list[i+1]=list[i+1],list[i]
-    return list
-list=[10,28,9,34,5,3,24]
-print(change_position(list))
+l=[1,2,3,4,5,6]
+for i in range(0,len(l),2):
+    if i<len(l):
+        l[i],l[i+1]=l[i+1],l[i]
+print(l)
