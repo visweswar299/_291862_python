8baa8b60e4f761e9444da36a6ce599f89d1a7787
visweswar <visweswar@299@gmail.com>
2021-04-23 09:21:04 +0530

    if-elif-else unsolved4 update

--- if-elif-else unsolved4.py
+++ if-elif-else unsolved4.py
@@ -1,9 +1,13 @@
 #write a program to check if the input number is
--real numbers
--float numbers
--string
--complex numbers
--Zer0(0)
+#-real numbers
+#-float numbers
+#-string
+#-complex numbers
+#-Zer0(0)
 
 value=input("Enter the value:")
-if()
+if(value>'0' and value.isnumeric()==True):
+    print("Real number")
+elif(value=='0'):
+    print("Zero")
+
