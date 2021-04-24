76f2eb3abb02170cd0e5569863a0a51e5d4c4db2
visweswar <visweswar@299@gmail.com>
2021-04-24 08:18:21 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return a+b
+    print(a+b)
+a=int(input("Enter the value:"))
+b=int(input("ENter the value:"))
+c=add(a,b)
+print(c)
+
+
+
+
+a=int(input("Enter the value:"))
+b=int(input("ENter the value:"))
+d=print(sum((a,b)))
