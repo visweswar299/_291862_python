918b69ca2543b9e66e52bd7ca3162d4b9cfc00d7
visweswar <visweswar@299@gmail.com>
2021-04-24 17:10:39 +0530

    comments update

---
+++ comments in python.py
@@ -0,0 +1,11 @@
+#comments
+"""Hi to all"""
+print("""Hi
+TO
+ALL""")
+print("""# tag is sible line comment in python""")
+
+
+
+print("""there is no any multi line comment in python but 
+we can use a multiline string.""")
