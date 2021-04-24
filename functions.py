c159de5226d1e60b917cbee2121f4703b4e80aa9
visweswar <visweswar@299@gmail.com>
2021-04-24 08:22:16 +0530

    functions and docstring

--- functions.py
+++ functions.py
@@ -1,11 +1,12 @@
 def add(a,b):
+    """this function returns sum of two numbers"""
     return a+b
     print(a+b)
 a=int(input("Enter the value:"))
 b=int(input("ENter the value:"))
 c=add(a,b)
 print(c)
-
+print(add.__doc__)
 
 
 
