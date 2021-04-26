951f826a8a91977af8a189009dc59384ba916ded
visweswar <visweswar@299@gmail.com>
2021-04-26 08:49:39 +0530

    Strings example

---
+++ String examples.py
@@ -0,0 +1,20 @@
+# access characters
+
+s = "Apple Juice"
+# first character
+print('s[0]=', s[0])
+# Last character
+print('s[-1=', s[-1])
+# slicing 2nd to 5th character
+print('s[1:5]=', s[1:5])
+# slicing 6th to 2nd character
+print('s[5:-2]=', s[5:-2])
+
+# Concatenation of two strings
+s = "Hello"
+s1 = "World!"
+print('s+s1=', s+s1)
+
+# repeat the given string 5 times
+s = "viswa"
+print('s*5=', s*5)
